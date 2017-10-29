4191bf3c68db250515503039a6d816f70e930185
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-30 03:14:19 +0900

    フォーローライトの実装

---
+++ Assets/Scripts/LigthController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigthController : MonoBehaviour
+{
+	[SerializeField]
+	GameObject player;
+
+	Vector3 lightDistance;
+
+    // Use this for initialization
+    void Start()
+    {
+		// 開始時のライトとプレイヤーの位置の差を取得
+		lightDistance = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// プレイヤーに常に初期のライトとの差を加える
+		transform.position = player.transform.position + lightDistance;
+    }
+}
