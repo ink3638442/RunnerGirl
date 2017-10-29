65821f1e8fb2689d7ff7168f0fb1abb284636b85
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-29 19:59:57 +0900

    フロア作成の実装

---
+++ Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour
+{
+	[SerializeField]
+	GameObject floorPrefab;
+
+    // Use this for initialization
+    void Start()
+    {	
+		for (int i = 0; i < 500; i++)
+		{
+			GameObject go = Instantiate(floorPrefab);
+			go.transform.position = new Vector3(i, 0, 0);
+
+
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
