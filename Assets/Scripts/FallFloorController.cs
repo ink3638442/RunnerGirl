abff5bfa4bf393dd52ce62a6e9df962c8582cd2d
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-30 16:15:13 +0900

    自動床落下の修正

--- Assets/Scripts/FallFloorController.cs
+++ Assets/Scripts/FallFloorController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FallFloorController : MonoBehaviour
 {
-	bool isFall = false;
-	float destroyDistance = 0;
+	bool isFall = false;　      // 落下作動スイッチ
+	float destroyDistance = 0;  // 削除判定対象用
 
     // Update is called once per frame
     void Update()
@@ -18,17 +18,23 @@ public class FallFloorController : MonoBehaviour
 		StartCoroutine(SetFallBool());
 	}
 
+	/// <summary>
+	/// 落下スイッチ切り替え 一定時間後作動
+	/// </summary>
 	IEnumerator SetFallBool()
 	{
 		yield return new WaitForSeconds(3);
 		isFall = true;
 	}
 
+	/// <summary>
+	/// 自動床落下 一定ラインでオブジェクトが破棄される
+	/// </summary>
 	void Fall()
 	{
 		transform.Translate(0, -0.9f * Time.deltaTime, 0);
-		destroyDistance += 0.8f * Time.deltaTime;
+		destroyDistance += 0.9f * Time.deltaTime;
 
-		if (destroyDistance > 2) Destroy(gameObject);
+		if (destroyDistance > 1.5f) Destroy(gameObject);
 	}
 }
