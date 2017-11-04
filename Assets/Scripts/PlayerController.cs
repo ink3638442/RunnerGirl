a25d4a3396fc0f8b0e3320482a2b3d5f01ba0056
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-11-05 00:20:12 +0900

    方向キーの修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,24 +22,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(KeyCode.DownArrow))
+		// カクカク曲がるようにGetAxisRawを使用　-1, 0, 1の値のみ
+		float vertical   = Input.GetAxisRaw("Vertical");
+		float horizontal = Input.GetAxisRaw("Horizontal");
+
+		if (vertical != 0)
 		{
-			transform.rotation = Quaternion.Euler(0, 0, 0);
+			int direction = vertical < 0 ? 0 : -180;
+			transform.rotation = Quaternion.Euler(0, direction, 0);
 			transform.Translate(Vector3.forward * 3 * Time.deltaTime);
 		}
-		if (Input.GetKey(KeyCode.UpArrow))
+		
+		if (horizontal != 0)
 		{
-			transform.rotation = Quaternion.Euler(0, -180, 0);
-			transform.Translate(Vector3.forward * 3 * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.rotation = Quaternion.Euler(0, 90, 0);
-			transform.Translate(Vector3.forward * 3 * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.rotation = Quaternion.Euler(0, -90, 0);
+			int direction = horizontal < 0 ? 90 : -90;
+			transform.rotation = Quaternion.Euler(0, direction, 0);
 			transform.Translate(Vector3.forward * 3 * Time.deltaTime);
 		}
 
